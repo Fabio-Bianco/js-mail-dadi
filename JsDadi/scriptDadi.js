c46239d62eeb260f93eb86e798f2f31fde78eb47
Fabio-Bianco <biancobfabio@gmail.com>
2025-02-28 11:29:43 +0100

    js-Mail-Dadi correction post lesson;

--- JsDadi/scriptDadi.js
+++ JsDadi/scriptDadi.js
@@ -1,19 +1,53 @@
-// Genera un numero casuale da 1 a 6
-function pullNut() {
-    return Math.floor(Math.random() * 6) + 1;
-}
+// // Genera un numero casuale da 1 a 6
+// function pullNut() {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
 
-let player = pullNut();
-let PcPlayer = pullNut();
+// let player = pullNut();
+// let PcPlayer = pullNut();
 
-console.log("Giocatore:", player);
-console.log("Computer:", PcPlayer);
+// console.log("Giocatore:", player);
+// console.log("Computer:", PcPlayer);
 
-// Determina il vincitore
+// // Determina il vincitore
+// if (player > PcPlayer) {
+//     console.log("Il giocatore vince!");
+// } else if (PcPlayer > player) {
+//     console.log("Il computer vince!");
+// } else {
+//     console.log("Pareggio!");
+// }
+
+// CORREZIONE ESERCIZIO;
+
+// Chiedo all'utente di inserire un munero da 1 a 6;
+const player = parseInt (prompt('Inserisci un numero da 1 a 6'));
+// Genero un numero numero da 1-6;
+// const player = Math.floor(Math.random() * 6) + 1;
+console.log (player);
+const PcPlayer = Math.floor(Math.random() * 6) + 1;
+console.log (PcPlayer);
+// Variabille di validazione;
+const isValid = player >= 1 && player <= 6 && !isNaN(player);
+console.log (isValid)
+// const userNumber =Math.round(Math.random() * 5) + 1;
+// const userNumber = Math.cell(Math.random() * 6);
+
+// Istruzioni condizionali
+if (isValid){
 if (player > PcPlayer) {
-    console.log("Il giocatore vince!");
-} else if (PcPlayer > player) {
-    console.log("Il computer vince!");
+        console.log("Il giocatore vince!");
+        alert ("Il giocatore vince!")
+    } else if (PcPlayer > player) {
+        console.log("Il computer vince!");
+        alert ("Il computer vince!")
+    } else {
+        console.log("Pareggio!");
+        alert ("Pareggio!")
+    }
 } else {
-    console.log("Pareggio!");
+
+    console.log('Mi dispiace! Stai barando');
+    alert ('Mi dispiace! Stai barando');
+
 }
--- JsMail/scriptMail.js
+++ JsMail/scriptMail.js
@@ -17,6 +17,7 @@ const userMail = prompt("Inserisci la tua email per accedere alla festa:");
 // Controllo se la mail è presente nella lista;
 let mailPresent = false;
 
+
 for (let i = 0; i < list.length; i++){
     if (list[i] === userMail)
         mailPresent = true;
@@ -26,11 +27,39 @@ for (let i = 0; i < list.length; i++){
 
 if (mailPresent){
     console.log("Benvenuto alla festa!");
+    alert ("Benvenuto alla festa!")
 } else {
     console.log("Spiacente, il tuo nome non è sulla lista.");
+    alert ("Spiacente, il tuo nome non è sulla lista.")
 }
 
+// Creo un Array con una lista di emails;
+// const list = [
+//     "michele.rossi123@mail.com",
+//     "marcello.bianchi87@gmail.com",
+//     "marco.verdi55@yahoo.it",
+//     "filippo.neri22@outlook.com",
+//     "luca.martini99@protonmail.com",
+//     "elena.ricci77@hotmail.com",
+//     "simone.ferri33@live.com",
+//     "alessia.gallo66@mail.it",
+//     "matteo.rinaldi11@tiscali.it",
+//     "chiara.mancini44@fastwebnet.it"
+//   ];
+// const userMail = prompt("Inserisci la tua email per accedere alla festa:");
+// let index = 0;
+// userFound = false;
 
+// let mailPresent = false;
+// while(index < list.length && userFound === false){
+//     if (list(index) === userMail){
+//      console.log("Benvenuto alla festa!");
+//      alert ("Benvenuto alla festa!");
+//      userFound = true;
+
+//     }
+//     index++
+// } 
 
 
 
